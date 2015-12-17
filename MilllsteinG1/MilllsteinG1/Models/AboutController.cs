86d4d45b99411c1945182e8726ffbbe5e312a817
dioscar_cleverdesign <Dioscar@cleverdesign.com>
2015-12-17 09:37:49 -0500

    --Added terms and conditions

---
+++ MilllsteinG1/MilllsteinG1/Models/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MilllsteinG1.Models
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult TermsAndConditions()
+        {
+            return View();
+        }
+    }
+}
