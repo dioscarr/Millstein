85d792c75d5e2dbea6abfc5673a5a32a9152bd8b
dioscar_cleverdesign <Dioscar@cleverdesign.com>
2015-12-18 09:10:20 -0500

    Update site

---
+++ MilllsteinG1/MilllsteinG1/Areas/Admin/Models/BaseModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL.Models;
+using BLL;
+
+namespace MilllsteinG1.Areas.Admin.Models
+{
+    public class BaseModel
+    {
+                public Contact ContantInfo = ManageContact.GetSingleContact();
+
+        public string Truncate(string text, int length = 200, string ellipsis = "...", bool keepFullWordAtEnd = true)
+        {
+            string txtWNoHTML = System.Text.RegularExpressions.Regex.Replace(text, @"<[^>]+>", "").Trim();
+
+            if (String.IsNullOrEmpty(txtWNoHTML))
+            {
+                return string.Empty;
+            }
+
+            if (txtWNoHTML.Length < length)
+            {
+                return txtWNoHTML;
+            }
+
+            txtWNoHTML = txtWNoHTML.Substring(0, length);
+
+            if (keepFullWordAtEnd)
+            {
+                txtWNoHTML = txtWNoHTML.Substring(0, txtWNoHTML.LastIndexOf(' '));
+            }
+
+            return txtWNoHTML + ellipsis;
+        }
+    }
+    }
