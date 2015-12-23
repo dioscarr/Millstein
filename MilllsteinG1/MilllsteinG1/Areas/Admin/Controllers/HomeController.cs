228b914de8bb51537ea6ab97e08a3e1477d5ad96
dioscar_cleverdesign <Dioscar@cleverdesign.com>
2015-12-23 10:36:59 -0500

    chagnes

--- MilllsteinG1/MilllsteinG1/Areas/Admin/Controllers/HomeController.cs
+++ MilllsteinG1/MilllsteinG1/Areas/Admin/Controllers/HomeController.cs
@@ -15,55 +15,16 @@ namespace MilllsteinG1.Areas.Admin.Controllers
         {
 
 
-            return View(new ImageViewModel());
+            return View(new HomeModel());
         }
 
-    [HttpPost]
-[ValidateAntiForgeryToken]
-public ActionResult Index(ImageViewModel model)
-{
-    //var validImageTypes = new string[]
-    //{
-    //    "image/gif",
-    //    "image/jpeg",
-    //    "image/pjpeg",
-    //    "image/png"
-    //};
-
-    //if (model.ImageUpload == null || model.ImageUpload.ContentLength == 0)
-    //{
-    //    ModelState.AddModelError("ImageUpload", "This field is required");
-    //}
-    //else if (!imageTypes.Contains(model.ImageUpload.ContentType))
-    //{
-    //    ModelState.AddModelError("ImageUpload", "Please choose either a GIF, JPG or PNG image.
-    //}
-
-    //if (ModelState.IsValid)
-    //{
-    //    var image = new Image
-    //    {C:\Users\Clever Design\Desktop\Work\DisocarRodriguez\MVC-Project\MillsteinGithub\MilllsteinG1\MilllsteinG1\Areas\Admin\Controllers\AdvisorsController.cs
-    //        Title = model.Title,
-    //        AltText = model.AltText,
-    //        Caption = model.Caption
-    //    }
-
-    //    if (model.ImageUpload != null && model.ImageUpload.ContentLength > 0)
-    //    {
-    //        var uploadDir = "~/uploads"
-    //        var imagePath = Path.Combine(Server.MapPath(uploadDir), model.ImageUpload.FileName);
-    //        var imageUrl = Path.Combine(uploadDir, model.ImageUpload.FileName);
-    //        model.ImageUpload.SaveAs(imagePath);
-    //        image.ImageUrl = imageUrl;
-    //    }
-
-    //    db.Create(image);
-    //    db.SaveChanges();
-    //    return RedirectToAction("Index");
-    //}
-
-    return View(model);
-}
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(HomeModel model)
+        {
+            var result = model.Update(model);
+            return View();
+        }
 
      
 
