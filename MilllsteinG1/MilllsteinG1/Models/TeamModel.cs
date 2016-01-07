04761c5dae7f0d7de10eab7ebd7a6534713927ff
Lewis Lin <linnobita@gmail.com>
2016-01-07 13:05:47 -0500

    Tean Models

--- MilllsteinG1/MilllsteinG1/Models/TeamModel.cs
+++ MilllsteinG1/MilllsteinG1/Models/TeamModel.cs
@@ -6,7 +6,7 @@ using MilllsteinG1.Models;
 using DAL.Models;
 using BLL;
 
- 
+
 namespace MilllsteinG1.Models
 {
     public class TeamModel : BaseModel
@@ -16,7 +16,7 @@ namespace MilllsteinG1.Models
         public Advisory advisory { get; set; }
         public TeamModel()
         {
-            teamList = ManageTeam.GetAllTeam().Where(c => c.Type == "advisor").ToList();
+            teamList = ManageTeam.GetAllTeam().Where(c => c.Type == "advisor").OrderBy(c => c.Order).ToList();
             advisory = ManageAdvisory.GetAllAdvisory().FirstOrDefault();
             team = null;
         }
